7d3e9352c18849772f8ea62d53b081da2afa4ff1
22BCS16868 <22BCS16868@cuchd.in>
2025-02-26 23:55:48 +0530

    88.cpp

---
+++ 88.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;  // Pointer for nums1
+        int j = n - 1;  // Pointer for nums2
+        int k = m + n - 1; // Pointer for the last position in nums1
+
+        // Merge from the back to avoid overwriting elements in nums1
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        // If nums2 still has elements left, copy them (nums1 already in place)
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+}
+
+Input
+nums1 =
+[1,2,3,0,0,0]
+m =
+3
+nums2 =
+[2,5,6]
+n =
+3
+Output
+[1,2,2,3,5,6]
+Expected
+[1,2,2,3,5,6]
